acca8aec3c4fa2321ead81362b232bb88274d2c3
ApplicationFramwork <84538168+ApplicationFramwork@users.noreply.github.com>
2021-05-29 19:12:05 +0530

    login url updated

--- src/service/ConferenceManagementSystemServices.js
+++ src/service/ConferenceManagementSystemServices.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import jwt_decord from "jwt-decode";
 //Login service URL
-const LOGIN_URL = "http://localhost:8070/user/login";
+const LOGIN_URL = "https://it19004778.d2n41wr3m7a1wa.amplifyapp.com/user/login";
 const ACCOUNT_DEACTIVATE_URL = "http://localhost:8070/user/delete";
 const ACCOUNT_UPDATE_URL = "http://localhost:8070/user/update";
 
