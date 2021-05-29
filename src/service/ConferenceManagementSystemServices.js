8b2cb3b445b1adb79c06193fb933a98f2c5348cf
ApplicationFramwork <84538168+ApplicationFramwork@users.noreply.github.com>
2021-05-29 19:24:09 +0530

    signOut updated

--- src/service/ConferenceManagementSystemServices.js
+++ src/service/ConferenceManagementSystemServices.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import jwt_decord from "jwt-decode";
 //Login service URL
 const LOGIN_URL = "https://it19004778.d2n41wr3m7a1wa.amplifyapp.com/user/login";
-const ACCOUNT_DEACTIVATE_URL = "http://localhost:8070/user/delete";
+const ACCOUNT_DEACTIVATE_URL = "https://it19004778.d2n41wr3m7a1wa.amplifyapp.com/user/delete";
 const ACCOUNT_UPDATE_URL = "https://it19004778.d2n41wr3m7a1wa.amplifyapp.com/user/update";
 
 class ConferenceManagementSystemServices{
